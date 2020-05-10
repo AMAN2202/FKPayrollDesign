cf4ec222cb22f810c963a0e9ab587e4519d037a7
Aman Yadav <amanyadav2202@gmail.com>
2020-05-10 13:18:49 +0530

    Added  Dao/DaoPersoanl_detail for handling crud database operation on persoan detail of employee 	TODO:: 		Error Handle
    _

---
+++ SourceCode/Dao/DaoPersoanl_detail.java
@@ -0,0 +1,154 @@
+package Dao;
+
+import entity.Personal_detail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaoPersoanl_detail {
+    static final private String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final private String DB_URL = "jdbc:mysql://localhost/db";
+    static final private String USER_NAME = "aman";
+    static final private String PASSWORD = "Lol@123456789@";
+    static Connection connection;
+    static Statement statement;
+    static ResultSet resultSet;
+
+
+    public static Personal_detail get_by_id(int id) {
+
+        String sql = "SELECT *  FROM Personal_detail where id=" + id;
+        List<Personal_detail> list= get_personal_detail(sql);
+        assert (list.size() == 1);
+        return list.get(0);
+    }
+
+    public static Personal_detail get_by_phoneno(String phoneno) {
+
+        String sql = String.format("SELECT *  FROM Personal_detail where phoneno='%s'",phoneno);
+        List<Personal_detail> list= get_personal_detail(sql);
+        assert (list.size() == 1);
+        return list.get(0);
+
+    }
+
+    public static List<Personal_detail> get_personal_detail(String sql) {
+        List<Personal_detail> list = new ArrayList<>();
+        try {
+            // load driver and get connection
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+
+            while (resultSet.next()) {
+//                System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2) + "\t" + resultSet.getInt(3) + "\t" + resultSet.getInt(4));
+                list.add(new Personal_detail(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3)));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+
+            }
+            return list;
+        }
+
+    }
+
+
+
+    public  static void add(Personal_detail p,int id)
+    {
+        String sql=String.format("insert into Personal_detail(id,name,address,phoneno) values(%d,'%s','%s','%s')",id,p.getName(),p.getAddress(),p.getPhoneno());
+        System.out.println(sql);
+
+        try {
+            // load driver and get connection
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+
+            }
+
+        }
+
+
+    }
+
+    public  static  void delete(Personal_detail p)
+    {
+        String sql=String.format("delete from Personal_detail where phoneno='%s'",p.getPhoneno());
+        System.out.println(sql);
+
+        try {
+            // load driver and get connection
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+
+            }
+
+        }
+
+
+    }
+
+    public  static  void update(Personal_detail p,int id)
+    {
+        String sql=String.format("update Personal_detail  set  phoneno='%s',address='%s' where id=%d",p.getPhoneno(),p.getAddress(),id);
+        System.out.println(sql);
+
+        try {
+            // load driver and get connection
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+
+            }
+
+        }
+
+    }
+
+
+}
--- SourceCode/entity/Emp.java
+++ SourceCode/entity/Emp.java
@@ -16,10 +16,10 @@ public class Emp {
 
     }
 
-    public Emp(Personal_detail Personal_detail, Union_detail Union_detail, Salary_detail Salary_detail) {
+    public Emp(Personal_detail Personal_detail, Union_detail Union_Detail, Salary_detail Salary_detail) {
         this.id=lastid+1;
         this.Personal_detail = Personal_detail;
-        this.Union_detail = Union_detail;
+        this.Union_detail = Union_Detail;
         this.Salary_detail = Salary_detail;
     }
 
@@ -28,23 +28,23 @@ public class Emp {
         return Personal_detail;
     }
 
-    public void setPersonal_detail(Personal_detail Personal_detail) {
-        this.Personal_detail = Personal_detail;
+    public void setPersonal_detail(Personal_detail Personal_Detail) {
+        this.Personal_detail = Personal_Detail;
     }
 
     public Union_detail getUnion_detail() {
         return Union_detail;
     }
 
-    public void setUnion_detail(Union_detail Union_detail) {
-        this.Union_detail = Union_detail;
+    public void setUnion_detail(Union_detail Union_Detail) {
+        this.Union_detail = Union_Detail;
     }
 
     public Salary_detail getSalary_detail() {
         return Salary_detail;
     }
 
-    public void setSalary_detail(Salary_detail Salary_detail) {
-        this.Salary_detail = Salary_detail;
+    public void setSalary_detail(Salary_detail Salary_Detail) {
+        this.Salary_detail = Salary_Detail;
     }
 }
